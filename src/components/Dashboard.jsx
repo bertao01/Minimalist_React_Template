c98b803177f3e30f482a39cdbe4be32765c5520f
bertao01 <victorbertaomonteiro@gmail.com>
2021-02-18 13:50:34 -0300

    Dashboard static graph and button component implementation

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
         <div className="jumbotron">
             <h1>Dashboard</h1>
             <p>Place dedicated to dashboard</p>
-            <img src="https://codinginfinite.com/wp-content/uploads/2019/08/love-programming.png" alt="Languages-popularity"/>
+            <img src="https://images.freeimages.com/images/large-previews/8c9/grapho-1236660.jpg" className="w-50" alt="Languages-popularity"/>
         </div>
     )
 }
